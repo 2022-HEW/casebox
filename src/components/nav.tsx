d826a2958e981b35884591ff7551c61f64e59981
hiroto2002 <hiroto2002h14@icloud.com>
2022-10-18 02:49:00 +0900

    linkのおためし

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,23 +1,33 @@
 import styles from "../styles/nav.module.css"
+import { useRouter } from'next/router'
+import Link from "next/link"
 
 const Nav =()=>{
-    
+    const router = useRouter()    
+
+    const back =()=>{
+        router.back()
+    }
     return(
         <div id={styles.container}>
         <div id={styles.wrapper}>
         <div id={styles.back}>
-            <p>戻る</p>
+            <p onClick={back}>戻る</p>
         </div>
         <div id={styles.display_box}>
             <div id={styles.disp}></div>
             <div id={styles.service_select}>
-                <div id={styles.select}><a href="./service_select.html">トップ</a></div>
-                <div id={styles.btn1}><a href="./template_select.php">テンプレ</a></div>
-                <div id={styles.btn2}><a href="./product_edit.html">オリジナル</a></div>
-                <div id={styles.btn3}><a href="./draw_edit.html">手書き</a></div>
-                <div id={styles.btn4}><a href="./rank.php">ランキング</a></div>
-                <div id={styles.btn5}><a href="./site_qr.html">サイト</a></div>
-                <div id={styles.btn6}><a href="./help.html">ヘルプ</a></div>
+                <Link href="/service_select">
+                    <div id={styles.select}><a href="./service_select.html">トップ</a></div>
+                </Link>
+                <Link href="/template_select">
+                    <div id={styles.btn1}><a >テンプレ</a></div>
+                </Link>
+                    <div id={styles.btn2}><a>オリジナル</a></div>
+                    <div id={styles.btn3}><a>手書き</a></div>
+                    <div id={styles.btn4}><a>ランキング</a></div>
+                    <div id={styles.btn5}><a>サイト</a></div>
+                    <div id={styles.btn6}><a>ヘルプ</a></div>
             </div>
         </div>
     </div>
