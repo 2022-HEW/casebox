3bfe2f367a60e5b1121902b22dd69775fc22a822
ShirakoPonZ <maple8495@gmail.com>
2022-10-19 15:13:34 +0900

    react-slick導入

--- src/components/slideShow.tsx
+++ src/components/slideShow.tsx
@@ -1,5 +1,26 @@
+import Slider from "react-slick";
+
+import styles from "../styles/SlideShow.module.css";
+
 const SlideShow = () => {
-    
+    const settings = {
+        
+    };
+
+    return(
+        <Slider {...settings}>
+            <div id={styles.slideshow}>
+                <ul className={styles.slider}>
+                    <li><img src="img/service/service_strap.png" alt=""/></li>
+                    <li><img src="img/service/service_strap.png" alt=""/></li>
+                    <li><img src="img/service/service_strap.png" alt=""/></li>
+                    <li><img src="img/service/service_strap.png" alt=""/></li>
+                    <li><img src="img/service/service_strap.png" alt=""/></li>
+                    <li><img src="img/service/service_strap.png" alt=""/></li>
+                </ul>
+            </div>
+        </Slider>
+    )
 }
 
 export default SlideShow;
