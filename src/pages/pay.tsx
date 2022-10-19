a5b0b16d69b986638351020efc68549615f5263c
HaradaRyo-hal <106656346+HaradaRyo-hal@users.noreply.github.com>
2022-10-19 18:01:32 +0900

    pay途中

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -1,11 +1,30 @@
-import Nav from "../components/Nav";
+import Nav from "../components/nav";
 import Box from "../components/Box";
+import styles from "../styles/pay.module.css";
+
 const pay =()=>{
     return(
-        <>
+        <div>     
+            <div id={styles.wrap}>
+                <div className={styles.block}>
+                    <p className={styles.write}>支払額</p>
+                    <p className={styles.money}>1500</p>
+                    <p className={styles.en}>円</p>
+                </div>
+                <div className={styles.block}>
+                    <p className={styles.write}>投入額</p>
+                    <p className={styles.money}>0</p>
+                    <p className={styles.en}>円</p>
+                </div>
+                <div className={styles.block}>
+                    <p className={styles.write}>おつり</p>
+                    <p className={styles.money}>0</p>
+                    <p className={styles.en}>円</p>
+                </div>
+            </div>
             <Box/>
             <Nav/>
-        </>
+        </div>
     )
 }
 
