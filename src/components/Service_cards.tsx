cd912d464bcc6a85cff732202ba899a47014a94a
ShirakoPonZ <maple8495@gmail.com>
2022-10-25 14:59:38 +0900

    サービスセレクト各カードの修正

--- src/components/Service_cards.tsx
+++ src/components/Service_cards.tsx
@@ -1,10 +1,36 @@
 import styles from '../styles/service_select.module.css'
-import { ReactNode } from 'react';
+import Link from "next/link";
+
+
+// 型定義
+type CardType ={
+    Card_link: string,
+    Card_name: string,
+    Card_desc: string
+}
 
 const Service_cards = ()=> {
     return(
         <>
-            
+            <Service_card Card_link={"./template_select.php"} Card_name={"テンプレートケース"} Card_desc={"もう既にデザインが仕上がっているケース"}/>
+            <Service_card Card_link={"./product_edit.html"} Card_name={"オリジナルケース"} Card_desc={"写真を自由に入れることができるオリジナルのケース"}/>
+            <Service_card Card_link={"./draw_edit.html"} Card_name={"手書きケース"} Card_desc={"自販機で自分で書いたイラストをケースできる"}/>
+        </>
+    )
+}
+
+const Service_card = ({ Card_link,Card_name,Card_desc }:CardType)=>{
+    return(
+        <>
+            <Link href={Card_link}>
+                <div>
+                <div className={styles.images}></div>
+                    <div className={styles.content}>
+                        <h3>{Card_name}</h3>
+                        <p>{Card_desc}</p>
+                    </div>
+                </div>
+            </Link>
         </>
     )
 }
--- src/pages/service_select.tsx
+++ src/pages/service_select.tsx
@@ -4,6 +4,7 @@
 import styles from '../styles/service_select.module.css'
 import Box from '../components/Box';
 import Nav from '../components/Nav';
+import Service_cards from '../components/Service_cards';
 // import CardsTemplate from '../components/CardsTemplate'
 // import CardsOriginal from '../components/CardsOriginal'
 // import CardsCreated from '../components/CardsCreated'
@@ -14,9 +15,9 @@ const Service = () => {
 
     return(
         <>
-        <Box index={false}/>
-        <Nav/>
-                    
+            <Box index={false}/>
+            <Nav/>
+            <Service_cards/>
         </>
     )
 }
