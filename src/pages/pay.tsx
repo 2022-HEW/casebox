bdf266b1f04e6484e9c0a2721948a39753d4187c
HaradaRyo-hal <106656346+HaradaRyo-hal@users.noreply.github.com>
2022-10-26 17:08:54 +0900

    pay途中

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -1,6 +1,8 @@
 import Nav from "../components/nav";
 import Box from "../components/Box";
 import styles from "../styles/pay.module.css";
+import Icon from "../../public/image/money.svg";
+
 
 const pay =()=>{
     return(
@@ -20,6 +22,9 @@ const pay =()=>{
                     <p className={styles.write}>おつり</p>
                     <p className={styles.money}>0</p>
                     <p className={styles.en}>円</p>
+                </div>
+                <div className={styles.select}>
+                    <img src={Icon}  alt="アイコン" />
                 </div>
             </div>
             <Box/>
