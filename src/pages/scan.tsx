42c10a13c9b710d11596f359939c8474c78f8799
hiroto2002 <hiroto2002h14@icloud.com>
2022-10-28 19:20:16 +0900

    QRrecoder

--- src/pages/scan.tsx
+++ src/pages/scan.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Box from '../components/common/Box'
 import Nav from '../components/common/Nav'
 import Tutorial_movie from '../components/Tutorial_movie'
- const Scan = () => {
+import Camera from '../components/Camera'
+ 
+const Scan = () => {
   return (
     <Box>
         <Nav>
-            
+            <Tutorial_movie/>
+            {/* <Camera/> */}
             <div>scan</div>
         </Nav>
     </Box>
