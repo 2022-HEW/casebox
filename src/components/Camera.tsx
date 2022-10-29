fcc4307cd36706d4dc78c910b76ee158b300adfc
hiroto2002 <hiroto2002h14@icloud.com>
2022-10-29 12:19:21 +0900

    qrcode読み取り完成

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import jsQR from 'jsqr';
+import { Stream } from 'stream';
+import { log } from 'console';
+import { useRouter } from 'next/router';
 
 const videoWidth: number = 750;
-const videoHeight: number = 1200;
+const videoHeight: number = 750;
 const videoFrameRate: number = 5;
 
 const constraints: MediaStreamConstraints = {
@@ -13,6 +16,7 @@ const constraints: MediaStreamConstraints = {
     frameRate: {
       max: videoFrameRate,
     },
+    // 内カメラ
     facingMode: {
       exact: 'user',
     },
@@ -20,28 +24,63 @@ const constraints: MediaStreamConstraints = {
 };
 
 const Camera = () => {
+    const router = useRouter()
   const videoRef = useRef<HTMLVideoElement>(null);
   const intervalRef = useRef<number>();
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [isContinue, setIsContinue] = useState(false);
+  const [isContinue, setIsContinue] = useState(true);
   const [qrCodeData, setQrCodeData] = useState<string[]>([]);
 
   useEffect(() => {
     const openCamera = async () => {
+        // <video>
+      const video = videoRef.current;
+      if (video) {
+        // video起動、リアルタイムで
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = stream;
+       
+    };
+    }
+    openCamera();
+    
+  }, []);
+
+  useEffect(()=>{
+    const openCamera = async () => {
+        // <video>
+        // video起動、リアルタイムで
       const video = videoRef.current;
       if (video) {
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        video.srcObject = stream;
-      }
+        if(!isContinue){
+            stream.getTracks().forEach(function(track:any) {
+                track.stop();
+            });        
+            // option を削除しなければカメラは消えない
+            
+            router.push({
+                pathname:"/device_select"
+            })
+            video.srcObject = null;
+            // console.log(video.srcObject);
+        }
     };
+    }
+    console.log(isContinue);
+    
     openCamera();
-  }, []);
+  },[isContinue])
 
+
+
+// 止める
   useEffect(() => {
     if (!isContinue) {
       return;
     }
 
+    // 2Dグラフィックを描画
     const decodeQRCode = () => {
       const context = canvasRef?.current?.getContext('2d');
       const video = videoRef?.current;
@@ -68,18 +107,35 @@ const Camera = () => {
     }, 1_000 / videoFrameRate);
     intervalRef.current = intervalId;
 
+    // const stopCamera = async () => {
+    //     // <video>
+    //     // video起動、リアルタイムで
+    //     const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    //     stream.getTracks().forEach(function(track:any) {
+    //         track.stop();
+    //         return
+    //     });
+    // }
+    // 見つけたら
+    if(qrCodeData[0]){
+        console.log(qrCodeData);
+        setIsContinue(false);
+        // router.push({
+        //     pathname:"/device_select"
+        // }
+        // )
+       
+        // stopCamera();
+                // return;
+    }
+
     return () => {
       clearInterval(intervalRef.current);
     };
+
+    
   }, [isContinue, qrCodeData]);
-
-  const handleStart = () => {
-    setIsContinue(true);
-  };
-
-  const handleStop = () => {
-    setIsContinue(false);
-  };
+// }, []);
 
   return (
     <div>
@@ -90,7 +146,7 @@ const Camera = () => {
             autoPlay
             playsInline={true}
             ref={videoRef}
-            style={{ width: '100%' }}
+            style={{ width: '20%' }}
           >
             <canvas width={videoWidth} height={videoHeight} ref={canvasRef} />
           </video>
@@ -99,8 +155,8 @@ const Camera = () => {
           <p>{qrCodeData.join('\n')}</p>
         </div>
         <div>
-          <button onClick={handleStart}>Start Scan</button>
-          <button onClick={handleStop}>Stop Scan</button>
+          {/* <button onClick={handleStart}>Start Scan</button>
+          <button onClick={handleStop}>Stop Scan</button> */}
         </div>
       </div>
     </div>
