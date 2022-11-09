e00b5f6c4b24d93f988b4c5eb85caf91f1594811
ShirakoPonZ <maple8495@gmail.com>
2022-11-09 16:28:16 +0900

    画像の大きさ変えた

--- src/components/common/SlideShow.tsx
+++ src/components/common/SlideShow.tsx
@@ -13,22 +13,21 @@ type imageType={
 }
 
 const SlideShow = () => {
+
+	// slick
     const settings = {
         // autoplay: true, //自動的に動き出すか。初期値はfalse。
         // infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-        // slidesToShow: 1,//スライドを画面に3枚見せる
-		// slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-		// // prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-		// // nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		// dots: true,//下部ドットナビゲーションの表示
+        // slidesToShow: 1,
+		// slidesToScroll: 1,
 		// arrow:true,
 
-		dots: true,
+		dots: true, //下部ドットナビゲーションの表示
 		infinite: true,
 		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrow: true,
+		slidesToShow: 1, //画面表示刺せるスライドの数
+		slidesToScroll: 1, //1回のスクロールで3枚の写真を移動して見せる
+		arrow: true, //
 	// 	responsive: [
 	// 		{
 	// 		breakpoint: 769,//モニターの横幅が769px以下の見せ方
@@ -48,16 +47,17 @@ const SlideShow = () => {
         
     };
 
+// SlideShow表示
     return(
 		<>
 			<div id={styles.slider}>
 				<Slider {...settings}>
-					<ImageDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
-					<ImageDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
-					<ImageDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
-					<ImageDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
-					<ImageDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
-					<ImageDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
+					<ImgDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
+					<ImgDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
+					<ImgDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
+					<ImgDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
+					<ImgDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
+					<ImgDesc imgPath={"/image/strap.svg"} imgAlt={"スマホストラップ"}/>
 				</Slider>
             </div>
 		</>
@@ -65,13 +65,11 @@ const SlideShow = () => {
     )
 }
 
-const ImageDesc = ({ imgPath,imgAlt }:imageType) => {
+const ImgDesc = ({ imgPath, imgAlt }:imageType) => {
 	return(
-		<>
-			<div className={styles.imgContainer}>
-				<Image className={styles.slideImg} src={imgPath} alt={imgAlt}/>
-			</div>
-		</>
+		<div className={styles.imgContainer}>
+			<Image className={styles.slideImg} src={imgPath} alt={imgAlt} width={525} height={270} />
+		</div>
 	)
 }
 
