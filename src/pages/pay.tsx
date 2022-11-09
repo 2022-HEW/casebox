16964de85d3844ddd1d8bb3f3c7761662a3d8753
okimotiyo <106656028+okimotiyo@users.noreply.github.com>
2022-11-09 17:45:18 +0900

    pay.tsx関連

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Cash from "../../public/image/money.svg";
 import trafic from "../../public/image/t_money.svg";
 import electric from "../../public/image/e_money.svg";
+import other from "../../public/image/other_money.svg";
 import { useRecoilValue } from "recoil";
 import { productState } from '../atoms/atoms';
 
@@ -16,17 +17,17 @@ const pay = () => {
 
                 <div id={styles.wrap}>
 
-                    <Price_result write="支払額" />
-                    <Price_result write="投入額" />
-                    <Price_result write="おつり" />
+                    <Price_result id={styles.price} write="支払額" />
+                    <Price_result id={styles.payed} write="投入額" />
+                    <Price_result id={styles.back} write="おつり" />
 
 
                     <div className={styles.buttons}>
 
                         <Buttons imgPath={Cash} name="現金" />
-                        <Buttons imgPath={electric} name="電子マネー" />
-                        <Buttons imgPath={trafic} name="交通系" />
-                        <Buttons imgPath={Cash} name="現金" />
+                        <Buttons imgPath={trafic} name="クレジットカード" />
+                        <Buttons imgPath={electric} name="交通系電子マネー" />
+                        <Buttons imgPath={other} name="その他" />
 
                     </div>
                 </div>
@@ -38,11 +39,12 @@ const pay = () => {
 
 interface pay_props {
     write: string;
+    id: string;
 }
-const Price_result = ({ write,  }: pay_props) => {
-    const {m_product_price} =useRecoilValue(productState)
+const Price_result = ({ write, id }: pay_props) => {
+    const { m_product_price } = useRecoilValue(productState)
     return (
-        <div className={styles.block2}>
+        <div className={styles.block2} id={id}>
             <p className={styles.write}>{write}</p>
             <p className={styles.money}>{m_product_price}</p>
             <p className={styles.en}>円</p>
@@ -57,9 +59,11 @@ interface btn_props {
 const Buttons = ({ imgPath, name }: btn_props) => {
 
     return (
-        <a className={styles.cash}>
-            <Image src={imgPath} alt="現金の画像" />
-            <p id={styles.btnname}>{name}</p>
+        <a className={styles.payType}>
+            <div className={styles.btnContent}>
+                <Image src={imgPath} alt="現金の画像" id={styles.image} />
+                <p id={styles.btnname}>{name}</p>
+            </div>
         </a>
     )
 }
