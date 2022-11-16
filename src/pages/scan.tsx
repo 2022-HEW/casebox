02d8aabe4ffd07e10be913597a6570894baf166a
HaradaRyo-hal <106656346+HaradaRyo-hal@users.noreply.github.com>
2022-11-16 15:20:54 +0900

    scan途中

--- src/pages/scan.tsx
+++ src/pages/scan.tsx
@@ -8,59 +8,49 @@ import styles from "../styles/scan.module.css";
 const Scan = () => {
   return (
     <Box>
-        <Nav>
-            <Movie movieUrl={""}/>
-            <div id={styles.wrap}>
-              <div id={styles.number}>
-              <form name="dentaku">
-                <table>
-                  <!-- 液晶画面部分 -->
-                  <tr>
-                    <td colspan="4">
-                      <input type="text" class="display" name="display" value="" disabled>
-                    </td>
-                  </tr>
-            
-                  <!-- 上から1段目（7~9＋÷） -->
-                  <tr>
-                    <td><input type="button" value="7" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="8" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="9" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="÷" class="operator" name="div_btn" onclick="get_calc(this)"></td>
-                  </tr>
-                    
-                  <!-- 上から2段目（4~6＋×） -->
-                  <tr>
-                    <td><input type="button" value="4" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="5" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="6" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="×" class="operator" name="multi_btn" onclick="get_calc(this)"></td>
-                  </tr>
-            
-                  <!-- 上から3段目（1~3＋-） -->
-                  <tr>
-                    <td><input type="button" value="1" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="2" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="3" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="-" class="operator" onclick="get_calc(this)"></td>
-                  </tr>
-            
-                  <!-- 上から4段目（0/C/=/+) -->
-                  <tr>
-                    <td><input type="button" value="0" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="C" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="=" class="equal" onclick="get_calc(this)"></td>
-                    <td><input type="button" value="+" class="operator" onclick="get_calc(this)"></td>
-                  </tr>
-            
-                </table>
-                </form>
-              </div>
-            </div>
-            <Camera/>
-            <div>scan</div>
-        </Nav>
+      <Nav>
+        <div id={styles.wrap}>
+          <form>
+            <table className={styles.table}>
+              <tr>
+                <th colspan="3">
+                  <div className={styles.output}>
+                  </div>
+                </th>
+                <th>
+                  <div className={styles.error}>
+                    <p></p>
+                  </div>
+                </th>
+              </tr>
+              <tr>
+                <td><input type="button" value="７" onClick="calc_run(7)"></td>
+                <td><input type="button" value="８" onClick="calc_run(8)"></td>
+                <td><input type="button" value="９" onClick="calc_run(9)"></td>
+                <td><input type="button" value="C" onclick = "cle()"></td>
+              </tr>
+              <tr>
+                <td><input type="button" value="４" onClick="calc_run(4)"></td>
+                <td><input type="button" value="５" onClick="calc_run(5)"></td>
+                <td><input type="button" value="６" onClick="calc_run(6)"></td>
+                <td><input type="button" value="←" onclick = "dele()"></td>
+              </tr>
+              <tr>
+                <td><input type="button" value="１" onClick="calc_run(1)"></td>
+                <td><input type="button" value="２" onClick="calc_run(2)"></td>
+                <td><input type="button" value="３" onClick="calc_run(3)"></td>
+                <td rowspan="2"><input type="button" value="検索" onClick="serch"></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td><input type="button" value="０" onClick="calc_run(0)"></td>
+              </tr>
+            </table>
+          </form>
+        </div>
+      </Nav>
     </Box>
   )
 }
+
 export default Scan
