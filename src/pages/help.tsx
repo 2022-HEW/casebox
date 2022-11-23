be0182975774444f22d7b5a3e85e32c92e31c5b4
hiroto2002 <hiroto2002h14@icloud.com>
2022-11-23 20:03:59 +0900

    add help design

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,15 +1,47 @@
 import React from 'react'
 import Box from '../components/common/Box'
 import Nav from '../components/common/Nav'
-
+import styles from "../styles/help.module.css"
+import Image from 'next/image'
 const Help = () => {
+    const answer:string[]=["CASEBOXチャットサポートです。どのようなことでお困りでしょうか"]
+    return (
+        <Box>
+            <Nav>
+                <Header/>
+                <Answer answer={answer[0]}></Answer>
+                <Questions/>
+            </Nav>
+        </Box>
+    )
+}
 
-return (
-    <Box>
-        <Nav>
-            
-        </Nav>
-    </Box>
-)
+
+const Header = ()=>{
+    return(
+        <div className={styles.header}>
+            <p>CASE BOX サポートチーム</p>
+        </div>
+    )
+}
+
+type Props={
+    answer:string
+}
+const Answer = ({answer}:Props)=>{
+    return(
+        <div className={styles.answer}>
+            <Image src={"/image/answer.svg"} width={70} height={70}/>
+            <p>{answer}</p>
+        </div>
+    )
+}
+
+const Questions = ()=>{
+    return(
+        <div className={styles.question}>
+            <span>CASE BOXについて</span>
+        </div>
+    )
 }
 export default Help 
