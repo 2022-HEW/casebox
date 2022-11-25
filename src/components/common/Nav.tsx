f7188c1f2c1eca9cd5c6f18964de32142424fa74
hiroto2002 <hiroto2002h14@icloud.com>
2022-11-25 16:53:33 +0900

    fix bag

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,8 +1,8 @@
 import styles from "../../styles/nav.module.css"
 import { useRouter } from'next/router'
 import Link from "next/link"
-import { ReactNode, useState,useMemo, useRef,} from 'react';
-import { useRecoilState,useRecoilValue } from "recoil";
+import { ReactNode, useState,useMemo, useRef, Dispatch,} from 'react';
+import { SetterOrUpdater, useRecoilState,useRecoilValue } from "recoil";
 import { productState,tabState,stepState,modalState,designState,imageState } from '../../atoms/atoms';
 import { useEffect } from "react";
 import { log } from "console";
@@ -20,6 +20,7 @@ type Props = {
 type Tab_type ={
     site_link:string;
     site_name:string;
+    setModal:SetterOrUpdater<boolean>
 }
 
 const Nav =({children}:Props)=>{
@@ -109,12 +110,12 @@ const Nav =({children}:Props)=>{
             {/* <img src="./Nav/disp.svg"/> */}
             <div id={styles.disp}>{children}</div>
             <div id={styles.service_select}>
-                <Tab site_link={"./service_select"} site_name={"トップ"}  />
-                <Tab site_link={"./template_select"} site_name={"テンプレ"}  />
-                <Tab site_link={"./scan"} site_name={"オリジナル"}  />
-                <Tab site_link={"./device_select"} site_name={"手書き"} />
-                <Tab site_link={"./site"} site_name={"公式サイト"}  />
-                <Tab site_link={"./help"} site_name={"ヘルプ"}  />
+                <Tab site_link={"./service_select"} site_name={"トップ"}  setModal={setModal}/>
+                <Tab site_link={"./template_select"} site_name={"テンプレ"}  setModal={setModal}/>
+                <Tab site_link={"./scan"} site_name={"オリジナル"}  setModal={setModal}/>
+                <Tab site_link={"./device_select"} site_name={"手書き"} setModal={setModal}/>
+                <Tab site_link={"./site"} site_name={"公式サイト"}  setModal={setModal}/>
+                <Tab site_link={"./help"} site_name={"ヘルプ"}  setModal={setModal}/>
             </div>
             {/* <div id={styles.back} onClick={back}>もどる</div> */}
             <Button label="もどる" situ_name={"nav"} onClick={back}/>
@@ -124,13 +125,13 @@ const Nav =({children}:Props)=>{
     )
 } 
 
-    const Tab = React.memo(function Memotab({site_link,site_name}:Tab_type){
+    const Tab = React.memo(function Memotab({site_link,site_name,setModal}:Tab_type){
     
         const[tab,setTab] = useRecoilState(tabState);
         
         return( 
             <Link href={site_link} as={site_link} passHref>
-                <div id={styles.btn} className={`${tab === site_name && styles.select}`} onClick={()=>{setTab(site_name);}}>
+                <div id={styles.btn} className={`${tab === site_name && styles.select}`} onClick={()=>{setTab(site_name);setModal(false);}}>
                     {site_name}
                 </div>
             </Link>
@@ -138,3 +139,4 @@ const Nav =({children}:Props)=>{
     })
 
 export default Nav;
+
