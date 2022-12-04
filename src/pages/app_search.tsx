bf666e7e0e1cee9945a6caca9738bd6a3e398717
hiroto2002 <hiroto2002h14@icloud.com>
2022-12-04 17:14:51 +0900

    add 人気順　

--- src/pages/app_search.tsx
+++ src/pages/app_search.tsx
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import styles from "../styles/app_search.module.css"
 import Image from 'next/image'
 import App_nav from '../components/common/App_nav'
 import useSWR from 'swr'
 
 const app_search = () => {
-
+    const [text,setText] = useState("")
     return (
         <div className={styles.container}>
-            <SearchHeader />
+            <SearchHeader setText={setText} text={text}/>
             <App_nav/>
-            <SearchResult/>
+            <SearchResult text={text}/>
         </div>
     )
 }
 
 
-type Header={
-    label:string
+type Props={
+    text:string
+    setText?:Dispatch<SetStateAction<string>>
 }
-const SearchHeader = () =>{
+const SearchHeader = ({text,setText}:Props) =>{
     return(
         <header className={styles.header}>
             <Image width={10} height={10} src={"/image/search.svg"}/>
-            <input type="text" name="search" value="" placeholder="探す" />
+            <input type="text" name="search" value={text} placeholder="探す" onChange={(e)=>{setText && setText(e.target.value)}}/>
         </header>
     )
 }
@@ -36,7 +37,7 @@ type Product ={
     m_product_category:string,
     m_product_price:number
   }
-const SearchResult = () =>{
+const SearchResult = ({text}:Props) =>{
 
     const [product, setProduct] = useState([])
     
@@ -51,8 +52,26 @@ const SearchResult = () =>{
         if(data){
             setProduct(data)
         }
+        // console.log(data);
     },[data])
 
+
+    // 降順sort
+    const Rank = () =>{
+        let newList:any = product.sort((el1:any,el2:any)=>{
+            if(el1.product_liked < el2.product_liked){
+                return 1;
+            }
+            if (el1.product_liked > el2.product_liked) {
+                return -1;
+            }
+            return 0
+        }) ;
+        // console.log(newList);
+        
+        
+    }
+    Rank()
         // 取得するまで
     //   if(!data) return (<Box><Nav><></></Nav></Box>)
     return(
@@ -64,15 +83,16 @@ const SearchResult = () =>{
             <div className={styles.result_box}>
                 <div className={styles.result_line}>
                     {product.map((product:Product,index:number) => (
-                    <ProductBox product_place={product.product_place}
-                                product_name={product.product_name}
-                                m_product_category={product.m_product_category}
-                                m_product_price={product.m_product_price}
-                                product_liked={product.product_liked}
-                                key={product.product_ID}
-                                product_ID={index}
-                                //   setProduct_ID={setProduct_ID}
-                    />
+                        product.product_name.includes(text) &&
+                            <ProductBox product_place={product.product_place}
+                                        product_name={product.product_name}
+                                        m_product_category={product.m_product_category}
+                                        m_product_price={product.m_product_price}
+                                        product_liked={product.product_liked}
+                                        key={product.product_ID}
+                                        product_ID={index}
+                                        //   setProduct_ID={setProduct_ID}
+                            />
                     ))}
                 </div>
             </div>
