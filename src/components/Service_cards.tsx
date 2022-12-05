bbabbfb565ea6cd7f12b0b224a45eda9949ca39d
ShirakoPonZ <maple8495@gmail.com>
2022-12-05 18:46:54 +0900

    構成変更

--- src/components/Service_cards.tsx
+++ src/components/Service_cards.tsx
@@ -2,6 +2,10 @@ import Image from 'next/image'
 import Link from "next/link";
 import styles from '../styles/service_select.module.css';
 
+import CardTemplate from "../../public/service_select/template_select.png";
+import CardOriginal from "../../public/service_select/original.png";
+import CardIllust from "../../public/service_select/illust.png";
+
 
 // 型定義
 type CardType ={
@@ -15,31 +19,48 @@ type imageType={
 	imgAlt: string,
 }
 
+const CARD_ITEM = [
+    {
+        href: "/template_select",
+        name: "テンプレートケース",
+        desc: "もう既にデザインが仕上がっているケース"
+    },
+    {
+        href: "/scan",
+        name: "オリジナルケース",
+        desc: "写真を自由に入れることができるオリジナルのケース"
+    },
+    {
+        href: "/device_select",
+        name: "手書きケース",
+        desc: "自販機で自分で書いたイラストをケースできる"
+    },
+]
+
+// const IMG_ITEM = [
+//     {
+//         imgPath: ,
+//         imgAlt: ,
+//     }
+// ]
+
 const Service_cards = ()=> {
     return(
         <>
             <div className={styles.Container}>
-                <Service_card Card_link={"/template_select"} Card_name={"テンプレートケース"} Card_desc={"もう既にデザインが仕上がっているケース"}/>
-                <Service_card Card_link={"/scan"} Card_name={"オリジナルケース"} Card_desc={"写真を自由に入れることができるオリジナルのケース"}/>
-                <Service_card Card_link={"/device_select"} Card_name={"手書きケース"} Card_desc={"自販機で自分で書いたイラストをケースできる"}/>
+                {/* 配列を繰り返し表示 */}
+                {CARD_ITEM.map(item => {
+                    return(
+                        <Link key={item.href} href={item.href}>
+                            <div className={styles.content}>
+                                <div className={styles.images}></div>
+                                <h3>{item.name}</h3>
+                                <p>{item.desc}</p>
+                            </div>
+                        </Link>
+                    );
+                })}
             </div>
-        </>
-    )
-}
-
-const Service_card = (
-            { Card_link,Card_name,Card_desc }:CardType,
-        )=>{
-
-    return(
-        <>
-            <Link href={Card_link}>
-                    <div className={styles.content}>
-                        <div className={styles.images}></div>
-                        <h3>{Card_name}</h3>
-                        <p>{Card_desc}</p>
-                    </div>
-            </Link>
         </>
     )
 }
