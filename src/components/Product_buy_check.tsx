160b2c4372cff9feba55ac8cc07592032e9fc4e7
okimotiyo <106656028+okimotiyo@users.noreply.github.com>
2022-12-07 15:57:18 +0900

    Product_buy_check_CSS

--- src/components/Product_buy_check.tsx
+++ src/components/Product_buy_check.tsx
@@ -32,7 +32,7 @@ const Product_buy_check = ({ image_path, design_path, type_name, color_name, pro
     return (
         <>
             <div className={styles.wrapper}>
-            <Image className={styles.bgImage} src={image_path} alt="スマホ" width={250} height={400} objectFit='contain' />
+            <Image className={styles.bgImage} src={image_path} alt="スマホ" width={300} height={430} objectFit='contain' />
 
             {design_path !== "/design/" &&
                 <div className={styles.design}>
@@ -48,7 +48,7 @@ const Product_buy_check = ({ image_path, design_path, type_name, color_name, pro
                 <h2 className={styles.price}>&yen;{product_price}(税込)</h2>
                 <div className={styles.choseQuantity}>
                     <span className={styles.count} onClick={() => { count > 1 && setCount(count - 1) }}>－</span>
-                    <span className={styles.quantity}><p className={styles.number}>{count}</p></span>
+                    <div className={styles.quantity}><p className={styles.quantity_text}>{count}<span>こ</span></p></div>
                     <span className={styles.count} onClick={() => { count < 6 && setCount(count + 1) }}>＋</span>
                 </div>
                 <div className={styles.go_pay}><Button situ_name="screen" label="購入へ" onClick={go_pay_select} /></div>
