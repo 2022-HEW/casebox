a75f969c27ec5850cceeeb28392222d956da0d39
hiroto2002 <hiroto2002h14@icloud.com>
2022-12-11 04:01:59 +0900

    add id

--- src/components/common/App_button.tsx
+++ src/components/common/App_button.tsx
@@ -9,17 +9,19 @@ import styles from "../../styles/app_button.module.css";
 type Props = {
   label: string,
   style?:CSSProperties
+  id?:string
 }&ComponentProps<"button">;
 
-export const Button= ({ label, onClick,disabled,style}:Props) => {
+export const Button= ({ label, onClick,disabled,style,id}:Props) => {
   return (
     <button
       // sizeが'small'の時: styles.buttonとstyles.smallが適用される
       // sizeが'small'ではない時: styles.buttonとstyles.largeが適用される
-      className={`${styles.button}`}
+      className={styles.button}
       onClick={onClick}
       disabled={disabled}
       style={style}
+      id={id}
     >
       {label}
     </button>
