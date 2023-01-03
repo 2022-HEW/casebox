15fbf3d715d1286ca6148886ec55dd78566690bd
hiroto2002 <hiroto2002h14@icloud.com>
2023-01-03 18:16:19 +0900

    feat:header

--- src/pages/App/app_original.tsx
+++ src/pages/App/app_original.tsx
@@ -1,30 +1,33 @@
-import { NextPage } from 'next'
-import dynamic from 'next/dynamic'
-import React, { useState } from 'react'
-import styles from "../styles/app_original.module.css"
-const App_image_edit = dynamic(() => import('../components/App_image_edit'), { ssr: false })
+import { NextPage } from "next";
+import dynamic from "next/dynamic";
+import React, { useState } from "react";
+import App_header from "../../components/common/App_header";
+import styles from "../../styles/app_original.module.css";
+const App_image_edit = dynamic(
+  () => import("../../components/App_image_edit"),
+  { ssr: false }
+);
 
+const app_original: NextPage = () => {
+  const [save, setSave] = useState(false);
 
- const app_original:NextPage = () => {
-     const [save,setSave] = useState(false)
-     
-  
-    const Tool_box =()=>{
-        return(
-            <div className={styles.tool_box}>
-                <button onClick={()=>setSave(true)}>保存</button>
-            </div>
-        )}
-
+  const Tool_box = () => {
     return (
-        <div className={styles.app_original}>
-            <div style={{width:"100%",height:"100px",background:"#aaa"}}>header</div>
-            <App_image_edit save={save}/>
-            <Tool_box/>
-        </div>
-  )
-}
+      <div className={styles.tool_box}>
+        <button onClick={() => setSave(true)}>保存</button>
+      </div>
+    );
+  };
 
+  return (
+    <>
+      <App_header label="オリジナル" />
+      <div className={styles.app_original}>
+        <App_image_edit save={save} />
+        <Tool_box />
+      </div>
+    </>
+  );
+};
 
-
-export default app_original
+export default app_original;
