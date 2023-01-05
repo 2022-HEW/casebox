0f4fb2ccef24de987e757d1870db7a4a2b3759ba
hiroto2002 <hiroto2002h14@icloud.com>
2023-01-06 01:25:13 +0900

    feat:pathname

--- src/components/common/App_header.tsx
+++ src/components/common/App_header.tsx
@@ -5,13 +5,23 @@ import Image from "next/image"
 import { useRouter } from 'next/router'
 type Props ={
     label?:string
+    pathname?:string
 }
 
-const App_header = ({label}:Props) => {
+const App_header = ({label,pathname}:Props) => {
     const router = useRouter()
+    const handleClickBack=()=>{
+      if(pathname){
+        router.push({
+          pathname:`${pathname}`
+        })
+      }else{
+        router.back()
+      }
+    }
   return (
     <div className={styles.header}>
-        <div className={styles.back} onClick={()=>{router.back()}}>
+        <div className={styles.back} onClick={handleClickBack}>
             <Image src={"/image/back.svg"} width={20} height={20}/>
         </div>
         <p>{label}</p>
