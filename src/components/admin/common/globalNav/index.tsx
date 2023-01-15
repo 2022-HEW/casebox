ade2605043d3a19b8708cdd00251f3a511a7f76a
hiroto2002 <hiroto2002h14@icloud.com>
2023-01-16 00:57:18 +0900

    feat:materialIcon

---
+++ src/components/admin/common/globalNav/index.tsx
@@ -0,0 +1,10 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+export const globalNav = () => {
+  return (
+    <Grid container direction={"column"}>
+      <Grid item></Grid>
+    </Grid>
+  );
+};
