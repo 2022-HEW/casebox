6fa92a2699d78232115907b7e1a4543e4f07c31f
hiroto2002 <hiroto2002h14@icloud.com>
2023-01-25 21:24:54 +0900

    feat:DB + AZure function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,33 +1,53 @@
-type Body =  {
-  [key:string]:string|number
-}
+import useSWR from "swr";
+
+type Body = {
+  [key: string]: string | number;
+};
 
 export const fetcher = async (
-    resource: RequestInfo,
-    init?: RequestInit,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ): Promise<any> => {
-    const res = await fetch(resource, init)
-  
-    if (!res.ok) {
-      const errorRes = await res.json()
-      const error = new Error(
-        errorRes.message ?? 'APIリクエスト中にエラーが発生しました',
-      )
-  
-      throw error
-    }
-  
-    return res.json()
+  resource: RequestInfo,
+  init?: RequestInit
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Promise<any> => {
+  const res = await fetch(resource, init);
+
+  if (!res.ok) {
+    const errorRes = await res.json();
+    const error = new Error(
+      errorRes.message ?? "APIリクエスト中にエラーが発生しました"
+    );
+
+    throw error;
   }
 
-export const InsertDB = async(url:string,body?:Body)=>{
-  await fetch(url,{
-    method:"POST",
+  return res.json();
+};
+
+export const InsertDB = async (url: string, body?: Body) => {
+  await fetch(url, {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json;charset=utf-8'
+      "Content-Type": "application/json;charset=utf-8",
     },
     // body:JSON.stringify(body)
-    body:JSON.stringify(body)
+    body: JSON.stringify(body),
+  });
+};
+
+export const InsertAzure = async (body: Body) => {
+  await fetch("/api/blob_strage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+};
+
+export const getCategory = ()=>{
+  const {data,error} = useSWR("/api/admin_sql?situ=getCategory",fetcher)
+  return({
+    categories:data,
+    CatchError:error
   })
 }
