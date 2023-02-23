ef8e3d4171c348feddbccac0cb62c17817191204
hiroto2002 <hiroto2002h14@icloud.com>
2023-02-23 21:20:55 +0900

    feat:privacyTerms

--- src/pages/App/app_terms.tsx
+++ src/pages/App/app_terms.tsx
@@ -1,18 +1,10 @@
 import { NextPage } from "next";
 import React, { useState } from "react";
-import App_header from "../../components/app/common/App_header";
-import styles from "../../styles/app/common/app_text.module.css";
+import PrivacyTerms from "../../components/app/common/PrivacyTerms";
 
 const app_terms: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <App_header label="利用規約" />
-      <div className={styles.textBox}>
-        <p>
-          この利用規約（以下，「本規約」といいます。）は，CASEBOX（以下，「当社」といいます。）がこのウェブサイト上で提供するサービス（以下，「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には，本規約に従って，本サービスをご利用いただきます。
-        </p>
-      </div>
-    </div>
+   <PrivacyTerms name={"terms"}/>
   );
 };
 
