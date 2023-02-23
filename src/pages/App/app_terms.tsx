10e6215681bc0a4664733daa26319fb94dffb444
hiroto2002 <hiroto2002h14@icloud.com>
2023-02-23 20:04:59 +0900

    feat:利用規約

--- src/pages/App/app_terms.tsx
+++ src/pages/App/app_terms.tsx
@@ -1,15 +1,19 @@
-import { NextPage } from 'next'
-import React, { useState } from 'react'
-import App_header from '../../components/common/App_header'
-import styles from '../../styles/app_login.module.scss';
+import { NextPage } from "next";
+import React, { useState } from "react";
+import App_header from "../../components/common/App_header";
+import styles from "../../styles/app/common/app_text.module.css";
 
-const app_terms:NextPage = () => {
+const app_terms: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <App_header label="利用規約" />
+      <div className={styles.textBox}>
+        <p>
+          この利用規約（以下，「本規約」といいます。）は，CASEBOX（以下，「当社」といいます。）がこのウェブサイト上で提供するサービス（以下，「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には，本規約に従って，本サービスをご利用いただきます。
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.Container}>
-            <App_header label='利用規約'/>
-        </div>
-    )
-}
-
-export default app_terms
+export default app_terms;
