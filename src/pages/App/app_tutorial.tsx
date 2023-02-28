53b9859b8febcaef55bc4e0201948083ce6e6f2b
hiroto2002 <hiroto2002h14@icloud.com>
2023-02-28 23:23:35 +0900

    feat:tutorial

--- src/pages/App/app_tutorial.tsx
+++ src/pages/App/app_tutorial.tsx
@@ -3,16 +3,25 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 import App_header from "../../components/app/common/App_header";
-import styles from"../../styles/app/app_tutorial.module.css"
+import styles from "../../styles/app/app_tutorial.module.css";
 
 const tutorial: NextPage = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <div className={styles.container}>
-      <App_header label="手書きチュートリアル"/>
-      <div className={styles.video}>
-        <video src={`/app/video/${router.query}.mp3`} autoPlay/>
+      <App_header label="手書きチュートリアル" />
+      <div>
+        <video
+          className={styles.video}
+          src={
+            router.query.situ
+              ? `/movie/${router.query.situ}.mp4`
+              : `/movie/app_draw_tutorial.mp4`
+          }
+          controls
+          style={{ width: "100%" }}
+        />
       </div>
     </div>
   );
