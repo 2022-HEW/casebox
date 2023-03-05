797ae1875695c2bfe3b231d6f3fb200160da0a8d
hiroto2002 <hiroto2002h14@icloud.com>
2023-03-05 20:53:59 +0900

    fix:bag

--- src/pages/main/service_select.tsx
+++ src/pages/main/service_select.tsx
@@ -1,24 +1,37 @@
 // Linkタグのhref属性は要変更
 // slick入れる
-import Box from '../../components/main/common/Box';
-import Nav from '../../components/main/common/Nav';
-import Service_cards from '../../components/main/service_select/Service_cards';
-import SlideShow from '../../components/main/service_select/SlideShow';
-import { NextPage } from 'next';
+import Box from "../../components/main/common/Box";
+import Nav from "../../components/main/common/Nav";
+import Service_cards from "../../components/main/service_select/Service_cards";
+import SlideShow from "../../components/main/service_select/SlideShow";
+import { NextPage } from "next";
+import { useRecoilState } from "recoil";
+import { productState } from "../../atoms/atoms";
+import { useEffect } from "react";
 
-
-const Service:NextPage = () => {
-
-    return(
-        <>
-            <Box>
-                <Nav >
-                    <SlideShow/>
-                    <Service_cards/>
-                </Nav>
-            </Box>
-        </>
-    )
-}
+const Service: NextPage = () => {
+  const [product, setProduct] = useRecoilState(productState);
+  useEffect(() => {
+    setProduct({
+      m_product_price: 2000,
+      product_ID: null,
+      product_name: "",
+      product_place: "",
+      model_id: 0,
+      quant: 1,
+      m_product_category: "",
+    });
+  }, []);
+  return (
+    <>
+      <Box>
+        <Nav>
+          <SlideShow />
+          <Service_cards />
+        </Nav>
+      </Box>
+    </>
+  );
+};
 
 export default Service;
